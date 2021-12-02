a9e1f3280f2e9d5875a1a5488d6820956f908b1f
Matt Hawkins <hawkins22@gmail.com>
2021-12-02 10:14:16 -0600

    Moving tests to actual rust tests

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,41 @@
 
 mod measurements;
 
-fn run(d : &Vec<u32>) {
-    println!("Increases={}", d.iter().enumerate()
+fn increases(d: &Vec<u32>) -> usize { 
+    d.iter().enumerate()
         .skip(1)
         .filter(|x| &d[x.0-1]  < x.1)
-        .count());
-    println!("3 window increases={}", d.iter().enumerate()
+        .count()
+}
+
+fn sliding_window_increases(d: &Vec<u32>) -> usize {
+    d.iter().enumerate()
         .skip(3)
         .filter(|x| (&d[x.0-3] + &d[x.0-2] + &d[x.0-1])  < (&d[x.0-2] + &d[x.0-1] + &d[x.0]))
-        .count());
+        .count()
 }
 
 fn main() {
-    println!("==== Test Data ====");
-    run(&measurements::get_test());
-    println!("==== Real Data ====");
-    run(&measurements::get());
+    let d = measurements::get();
+    println!("Increases={}", increases(&d));
+    println!("Sliding Window Increases={}", sliding_window_increases(&d));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn test_d() -> Vec<u32> {
+        vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    }
+
+    #[test]
+    fn test_increases() {
+        assert_eq!(increases(&test_d()), 7)
+    }
+
+    #[test]
+    fn test_sliding_window_increases() {
+        assert_eq!(sliding_window_increases(&test_d()), 5)
+    }
 }
